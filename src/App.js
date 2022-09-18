b2b5a4e546533a1a6539c8662e0d609e3627aa8e
vokur13 <vokur13@gmail.com>
2022-09-18 13:57:45 +0300

    markup fixed with children

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import PaintingList from './components/PaintingList';
+import Section from './components/Section';
 import paintings from './json/paintings.json';
 
 export default function App() {
   return (
     <div>
-      <PaintingList items={paintings} />
+      <Section title="Top of the week">
+        <PaintingList items={paintings} />
+      </Section>
+      <Section title="The best of">
+        <PaintingList items={paintings} />
+      </Section>
     </div>
   );
 }
